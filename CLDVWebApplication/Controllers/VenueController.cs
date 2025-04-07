18caa8e78ab699cc0715a882854a278861eb1ac3
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-04-07 18:02:25 +0200

    Part 1

---
+++ CLDVWebApplication/Controllers/VenueController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CLDVWebApplication.Models;
+
+namespace CLDVWebApplication.Controllers
+{
+    public class VenueController : Controller
+    {
+        private readonly EventEaseDbContext _context;
+
+        public VenueController(EventEaseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Venue
+        public async Task<IActionResult> Index()
+        {
+            var venues = await _context.Venues.ToListAsync();
+            return View(venues);
+        }
+
+        // GET: Venue/Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // GET: Venue/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Venue/Create       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VenueId,VenueName,Location,Capacity,ImageUrl")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        // GET: Venue/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+            return View(venue);
+        }
+
+        // POST: Venue/Edit/5        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VenueId,VenueName,Location,Capacity,ImageUrl")] Venue venue)
+        {
+            if (id != venue.VenueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VenueExists(venue.VenueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        // GET: Venue/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // POST: Venue/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue != null)
+            {
+                _context.Venues.Remove(venue);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VenueExists(int id)
+        {
+            return _context.Venues.Any(e => e.VenueId == id);
+        }
+    }
+}
